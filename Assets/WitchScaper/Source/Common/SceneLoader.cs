ae01a7995486a0e3505f81231f0464ae00d4c0e2
g.khokhlov <g.khokhlov@corp.nekki.ru>
2021-01-10 18:34:18 +0300

    sceneLoader

---
+++ Assets/WitchScaper/Source/Common/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+namespace WitchScaper.Common
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        [SerializeField] private string _sceneName;
+
+        private void Start()
+        {
+            SceneManager.LoadScene(_sceneName);
+        }
+    }
+}
